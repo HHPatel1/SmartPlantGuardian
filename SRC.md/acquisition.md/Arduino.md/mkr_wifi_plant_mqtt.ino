be05ca3281b710e804845be5f7408e8be59358fa
HHPatel1 <hharrispatel@gmail.com>
2025-12-02 22:47:50 +0000

    Update mkr_wifi_plant_mqtt.ino

--- SRC.md/acquisition.md/Arduino.md/mkr_wifi_plant_mqtt.ino
+++ SRC.md/acquisition.md/Arduino.md/mkr_wifi_plant_mqtt.ino
@@ -1,1 +1,45 @@
 
+#include <WiFiNINA.h>
+#include <ArduinoMqttClient.h>
+
+char ssid[] = "HomeNetwork";
+char pass[] = "HomePassword";
+
+const char broker[] = "test.mosquitto.org";
+int port = 1883;
+const char topic[] = "smartplant/readings";
+
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  // Connect WiFi
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    delay(2000);
+  }
+
+  // Connect MQTT
+  while (!mqttClient.connect(broker, port)) {
+    delay(5000);
+  }
+
+  Serial.println("MKR WiFi MQTT Publisher Ready");
+}
+
+void loop() {
+  if (!mqttClient.connected()) {
+    mqttClient.connect(broker, port);
+  }
+
+  // Forward serial JSON from NICLA
+  if (Serial.available()) {
+    String json = Serial.readStringUntil('\n');
+    mqttClient.beginMessage(topic);
+    mqttClient.print(json);
+    mqttClient.endMessage();
+  }
+}
+
