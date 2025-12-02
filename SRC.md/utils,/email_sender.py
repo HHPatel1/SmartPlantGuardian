2eb84d18d08a14b5871bb50f29b209ca137c0e75
HHPatel1 <hharrispatel@gmail.com>
2025-12-02 23:07:25 +0000

    Update email_sender.py

--- SRC.md/utils,/email_sender.py
+++ SRC.md/utils,/email_sender.py
@@ -1,1 +1,16 @@
 
+import smtplib
+from email.message import EmailMessage
+
+def send_alert(to, subject, message):
+    msg = EmailMessage()
+    msg["Subject"] = subject
+    msg["From"] = "smartplantlabs@outlook.com"
+    msg["To"] = to
+    msg.set_content(message)
+
+    with smtplib.SMTP("smtp-mail.outlook.com", 587) as s:
+        s.starttls()
+        s.login("smartplantlabs@outlook.com", "YOUR_PASSWORD")
+        s.send_message(msg)
+
